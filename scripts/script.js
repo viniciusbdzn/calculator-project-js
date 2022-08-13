d9e25d6282ed3d430dd47859f9a3c3f1bd9b0c0e
Luiz Vinícius Badazoni <viniciusbdzn@gmail.com>
2022-08-13 17:19:38 -0300

    calculator project with js

---
+++ scripts/script.js
@@ -0,0 +1,137 @@
+
+const calculator = document.querySelector('.calculator');
+const display = calculator.querySelector('.calculator-display');
+const keys = calculator.querySelector('.calculator-keys');
+
+const calculate = (n1, operator, n2) => {
+    const firstNum = parseFloat(n1);
+    const secondNum = parseFloat(n2);
+    
+    if (operator === 'add') return firstNum + secondNum;
+    if (operator === 'sub') return firstNum - secondNum;
+    if (operator === 'mult') return firstNum * secondNum;
+    if (operator === 'divide') { 
+        if (secondNum === 0) {
+            return "Division by zero.";
+        }
+        return firstNum / secondNum;
+    }
+};
+
+const getKeyType = key => {
+    const { action } = key.dataset;
+    if (!action) return 'number'
+    if (
+        action === 'add' ||
+        action === 'sub' ||
+        action === 'mult' ||
+        action === 'divide'
+    ) return 'operator';
+    
+    return action;
+};
+
+
+const createResultString = (key, displayedNum, state) => {
+    const keyContent = key.textContent;
+    const keyType = getKeyType(key);
+    const {
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType
+    } = state;
+
+    if (keyType === 'number') {
+        return displayedNum === '0'||
+            previousKeyType === 'operator' ||
+            previousKeyType === 'calculate'
+            ? keyContent
+            : displayedNum + keyContent
+    };
+
+    if (keyType === 'decimal') {
+        if (!displayedNum.includes('.')) return displayedNum + '.';
+        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.';
+        return displayedNum;
+    };
+
+    if (keyType === 'operator') {
+        return firstValue &&
+            operator &&
+            previousKeyType !== 'operator' &&
+            previousKeyType !== 'calculate'
+            ? calculate(firstValue, operator, displayedNum)
+            : displayedNum
+    };
+
+    if (keyType === 'clear') return 0;
+
+    if (keyType === 'calculate') {
+        return firstValue
+            ? previousKeyType === 'calculate' 
+                ? calculate(displayedNum, operator, modValue)
+                : calculate(firstValue, operator, displayedNum)
+            : displayedNum
+    };
+};
+    
+
+const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+    const keyType = getKeyType(key);
+    const {
+        firstValue,
+        operator,
+        modValue,
+        previousKeyType
+    } = calculator.dataset;
+    
+    calculator.dataset.previousKeyType = keyType;
+
+    if (keyType === 'operator') {
+        calculator.dataset.operator = key.dataset.action;
+        calculator.dataset.firstValue = firstValue &&
+            operator &&
+            previousKeyType !== 'operator' &&
+            previousKeyType !== 'calculate'
+            ? calculatedValue
+            : displayedNum
+    };
+
+    if (keyType === 'calculate') {
+        calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
+            ? modValue
+            : displayedNum
+    };
+
+    if (keyType === 'clear' && key.textContent === 'AC') {
+        calculator.dataset.firstValue = '';
+        calculator.dataset.modValue = '';
+        calculator.dataset.operator = '';
+        calculator.dataset.previousKeyType = '';
+    };
+};
+
+const updateVisualState = (key, calculator) => {
+    const keyType = getKeyType(key);
+    
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
+
+    if (keyType === 'operator') key.classList.add('is-depressed');if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC';
+    if (keyType !== 'clear') {
+        const clearButton = calculator.querySelector('[data-action=clear]')
+        clearButton.textContent = 'CE';
+    }
+};
+
+//listening to the keys
+keys.addEventListener('click', e => {
+    if (!e.target.matches('button')) return
+        const key = e.target; //make something
+        const displayedNum = display.textContent;
+        const resultString = createResultString(key, displayedNum, calculator.dataset);
+
+        display.textContent = resultString;
+        updateCalculatorState(key, calculator, resultString, displayedNum);
+        updateVisualState(key, calculator);
+});
